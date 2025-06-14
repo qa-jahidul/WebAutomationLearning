c274964ed0d2e8d21b13ee2036da41efe8e4de8e
qa-jahidul <qa.jahidul@gmail.com>
2025-06-14 23:51:33 +0600

    google.com open

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AppTest {
     public static void main(String[] args){
         WebDriver driver = new ChromeDriver();
-
+        driver.get("https://google.com");
+        driver.close();
     }
 }
