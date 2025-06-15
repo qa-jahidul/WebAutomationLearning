5eaad8b69dc204987e022aedce493a76d1b73b0e
qa-jahidul <qa.jahidul@gmail.com>
2025-06-15 21:41:24 +0600

    window maximize added method added

---
+++ src/test/java/HandleBrowserRelatedTask.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+public class HandleBrowserRelatedTask extends DriverSetup {
+    @Test
+    public void testBrowserTask() throws InterruptedException {
+        driver.get("https://www.google.com");
+
+        String title = driver.getTitle();
+        System.out.println("Title: " + title);
+
+        String url = driver.getCurrentUrl();
+        System.out.println("URL: "+ url);
+
+        Thread.sleep(3000);
+
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+
+
+
+
+    }
+
+}
