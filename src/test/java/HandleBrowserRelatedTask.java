fd44927c71a8b5f5d9b3c7c053925cb4026b30a6
qa-jahidul <qa.jahidul@gmail.com>
2025-06-15 22:57:31 +0600

    navigations added

--- src/test/java/HandleBrowserRelatedTask.java
+++ src/test/java/HandleBrowserRelatedTask.java
@@ -11,15 +11,27 @@ public class HandleBrowserRelatedTask extends DriverSetup {
         String url = driver.getCurrentUrl();
         System.out.println("URL: "+ url);
 
-        Thread.sleep(3000);
+        Thread.sleep(1000);
 
         driver.manage().window().maximize();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
 
         driver.manage().window().minimize();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
 
         driver.manage().window().fullscreen();
+        Thread.sleep(1000);
+
+        driver.navigate().to("https://www.facebook.com");
+        Thread.sleep(3000);
+
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        driver.navigate().forward();
+        Thread.sleep(3000);
+
+        driver.navigate().refresh();
         Thread.sleep(3000);
 
 
