d75c21d4f53b166fbc6bf9d2a0749fd89916ae96
qa-jahidul <qa.jahidul@gmail.com>
2025-06-18 10:08:08 +0600

    get text command added

--- src/test/java/InteractingWithWebElement.java
+++ src/test/java/InteractingWithWebElement.java
@@ -1,2 +1,16 @@
-public class InteractingWithWebElement {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class InteractingWithWebElement extends DriverSetup{
+    @Test
+    public void interactWithElement(){
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        WebElement head = driver.findElement(By.xpath("//h1[normalize-space()='Practice Page']"));
+
+        String headText = head.getText();
+        System.out.println(headText);
+
+    }
 }
