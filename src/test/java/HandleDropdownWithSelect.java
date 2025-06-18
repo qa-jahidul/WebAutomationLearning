337e841dbd5c786a23eb4dbb3a507cb390bba336
qa-jahidul <qa.jahidul@gmail.com>
2025-06-18 17:34:07 +0600

    HandleDropdownWithSelect class added

---
+++ src/test/java/HandleDropdownWithSelect.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class HandleDropdownWithSelect extends DriverSetup{
+    @Test
+    public void interactWithDropdown() throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+    }
+}
