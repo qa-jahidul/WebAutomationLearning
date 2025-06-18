81e7d8a9e1e491c3916c9b6d7fe3cf6165731ee6
qa-jahidul <qa.jahidul@gmail.com>
2025-06-18 17:53:39 +0600

    select by index added

--- src/test/java/HandleDropdownWithSelect.java
+++ src/test/java/HandleDropdownWithSelect.java
@@ -1,8 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HandleDropdownWithSelect extends DriverSetup{
     @Test
     public void interactWithDropdown() throws InterruptedException {
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        WebElement selectElement = driver.findElement(By.id("dropdown-class-example"));
+        Select select = new Select(selectElement);
+
+        selectElement.click();
+        Thread.sleep(3000);
+
+        select.selectByIndex(2);
+        Thread.sleep(3000);
     }
 }
