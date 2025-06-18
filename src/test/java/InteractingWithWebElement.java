baa19a0cf52c8e03596b3cf960d64b1951a8caf3
qa-jahidul <qa.jahidul@gmail.com>
2025-06-18 10:50:07 +0600

    click command added

--- src/test/java/InteractingWithWebElement.java
+++ src/test/java/InteractingWithWebElement.java
@@ -4,13 +4,17 @@ import org.testng.annotations.Test;
 
 public class InteractingWithWebElement extends DriverSetup{
     @Test
-    public void interactWithElement(){
+    public void interactWithElement() throws InterruptedException {
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
         WebElement head = driver.findElement(By.xpath("//h1[normalize-space()='Practice Page']"));
 
         String headText = head.getText();
         System.out.println(headText);
+
+        WebElement radiobtn2 = driver.findElement(By.cssSelector("input[value='radio2']"));
+        radiobtn2.click();
+        Thread.sleep(3000);
 
     }
 }
