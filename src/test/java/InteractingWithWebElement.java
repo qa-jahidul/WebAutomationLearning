55d75c43992d95bf038758783d80f505b078e3ce
qa-jahidul <qa.jahidul@gmail.com>
2025-06-18 11:01:21 +0600

    sendkeys command added

--- src/test/java/InteractingWithWebElement.java
+++ src/test/java/InteractingWithWebElement.java
@@ -14,7 +14,11 @@ public class InteractingWithWebElement extends DriverSetup{
 
         WebElement radiobtn2 = driver.findElement(By.cssSelector("input[value='radio2']"));
         radiobtn2.click();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector("#name")).sendKeys("Jahidul");
+        Thread.sleep(2000);
+
 
     }
 }
